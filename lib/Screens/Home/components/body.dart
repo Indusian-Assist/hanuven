fda21814be2f20915aa0aaeb6566c81c80175ab3
tanishksahni <tanishk.sahni2002@gmail.com>
2023-05-11 19:26:55 +0530

    started appbar

--- lib/Screens/Home/components/body.dart
+++ lib/Screens/Home/components/body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hanuven/utils/constants/color.dart';
 
 import '../../../utils/constants/images_icons.dart';
 
@@ -9,10 +10,37 @@ class Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Image(
-        image: kDefaultImage,
-        fit: BoxFit.cover,
+    return Scaffold(
+      backgroundColor: kBackgroundColor,
+      appBar: AppBar(
+        backgroundColor: kBackgroundColor,
+        elevation: 0,
+        leading: Container(
+          margin: const EdgeInsets.all(4),
+          padding: const EdgeInsets.all(4),
+          decoration: BoxDecoration(
+            color: Colors.black,
+            borderRadius: BorderRadius.circular(25),
+          ),
+          child: IconButton(
+            icon: const Icon(Icons.arrow_back),
+            onPressed: () {
+              //TODO s
+            },
+          ),
+        ),
+        actions: [
+          Container(
+            child: IconButton(
+              icon: Image.asset(
+                kMainIconUrl,
+              ),
+              onPressed: () {
+                //TODO
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
