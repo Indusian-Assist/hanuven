7a50fd001ef08b16c2f343e143792a3c4e43047c
tanishksahni <tanishk.sahni2002@gmail.com>
2023-06-30 18:12:56 +0530

    fixed stack bugs in edit profile screen

--- lib/Screens/ProfileScreen/Components/edit_profile.dart
+++ lib/Screens/ProfileScreen/Components/edit_profile.dart
@@ -15,188 +15,231 @@ class _EditProfileState extends State<EditProfile> {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return Scaffold(
-      body: SingleChildScrollView(
-        scrollDirection: Axis.vertical,
-        child: Stack(
-          children: [
-            ClipPath(
-              clipper: CustomClipPath(),
-              child: Container(
-                height: size.height / 4,
-                width: double.maxFinite,
-                decoration: BoxDecoration(
-                    image: DecorationImage(
-                        image: kProfileBgImage, fit: BoxFit.cover)),
-              ),
-            ),
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 25, vertical: 20),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Container(
-                        margin: const EdgeInsets.all(3),
+    return SafeArea(
+      child: Scaffold(
+        body: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Stack(
+            children: [
+              Container(
+                child: Stack(
+                  children: [
+                    ClipPath(
+                      clipper: CustomClipPath(),
+                      child: Container(
+                        height: size.height / 4,
+                        width: double.maxFinite,
                         decoration: BoxDecoration(
-                          color: Colors.white.withOpacity(0.5),
-                          borderRadius: BorderRadius.circular(25),
+                            image: DecorationImage(
+                                image: kProfileBgImage, fit: BoxFit.cover)),
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.symmetric(horizontal: 25),
+                      alignment: Alignment.bottomRight,
+                      height: size.height / 4,
+                      child: Container(
+                        margin: EdgeInsets.symmetric(vertical: 5.0),
+                        decoration: BoxDecoration(
+                          color: kBackgroundColor.withOpacity(0.5),
+                          borderRadius: BorderRadius.circular(15),
                         ),
                         child: IconButton(
-                          icon: const Icon(Icons.arrow_back_ios_new),
                           onPressed: () {
-                            Navigator.pop(context);
+                            //Todo: Perform an action to edit background image
                           },
+                          icon: Icon(Icons.image),
+                          color: kDarkColor,
                         ),
                       ),
-                      //error
-                      Container(
-                        decoration: BoxDecoration(
-                          color: kBackgroundColor,
-                          borderRadius: BorderRadius.circular(25),
-                        ),
-                        child: IconButton(
-                          onPressed: () {
-                            Navigator.pushReplacementNamed(context, '/home');
-                          },
-                          icon: Image.asset(
-                            kMainIconUrl,
-                            height: 50,
-                            width: 50,
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 25, vertical: 20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          margin: const EdgeInsets.all(3),
+                          decoration: BoxDecoration(
+                            color: kBackgroundColor.withOpacity(0.5),
+                            borderRadius: BorderRadius.circular(25),
+                          ),
+                          child: IconButton(
+                            icon: const Icon(Icons.arrow_back_ios_new),
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
                           ),
                         ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(
-                    height: 90,
-                  ),
-                  Container(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Stack(
-                          children: [
-                            Container(
-                              margin: EdgeInsets.all(2.0),
-                              height: 150,
-                              width: 150,
-                              decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(100),
-                                  image: DecorationImage(
-                                      image: kProfileImage,
-                                      fit: BoxFit.cover,
-                                      opacity: 0.8)),
+                        //error
+                        Container(
+                          decoration: BoxDecoration(
+                            color: kBackgroundColor,
+                            borderRadius: BorderRadius.circular(25),
+                          ),
+                          child: IconButton(
+                            onPressed: () {
+                              Navigator.pushReplacementNamed(context, '/home');
+                            },
+                            icon: Image.asset(
+                              kMainIconUrl,
+                              height: 50,
+                              width: 50,
                             ),
-                            IconButton(
-                                onPressed: () {}, icon: Icon(Icons.edit)),
-                          ],
+                          ),
                         ),
                       ],
                     ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.symmetric(vertical: 20),
-                    child: Form(
-                      key: formKey,
-                      child: Container(
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            //name
-                            Padding(
-                              padding: EdgeInsets.symmetric(vertical: 5),
-                              child: TextFormField(
-                                decoration: InputDecoration(
-                                  border: OutlineInputBorder(),
-                                  hintText: 'Name',
-                                  labelText: 'Name',
+                    SizedBox(
+                      height: 90,
+                    ),
+                    Container(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Stack(
+                            children: [
+                              Container(
+                                margin: EdgeInsets.all(2.0),
+                                height: 150,
+                                width: 150,
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(100),
+                                    image: DecorationImage(
+                                      image: kProfileImage,
+                                      fit: BoxFit.cover,
+                                    )),
+                              ),
+                              Container(
+                                alignment: Alignment.center,
+                                height: 150,
+                                width: 150,
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    color: kBackgroundColor.withOpacity(0.7),
+                                    borderRadius: BorderRadius.circular(25),
+                                  ),
+                                  child: IconButton(
+                                    onPressed: () {
+                                      //Todo: perform an action to edit profile image
+                                    },
+                                    icon: Icon(Icons.edit),
+                                    color: kDarkColor,
+                                  ),
                                 ),
                               ),
-                            ),
-                            SizedBox(
-                              height: 10,
-                            ),
-                            //location
-                            Padding(
-                              padding: EdgeInsets.symmetric(vertical: 5),
-                              child: TextFormField(
-                                decoration: InputDecoration(
-                                  border: OutlineInputBorder(),
-                                  hintText: 'Location',
-                                  labelText: 'Location',
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(vertical: 20),
+                      child: Form(
+                        key: formKey,
+                        child: Container(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              //name
+                              Padding(
+                                padding: EdgeInsets.symmetric(vertical: 5),
+                                child: TextFormField(
+                                  decoration: InputDecoration(
+                                    border: OutlineInputBorder(),
+                                    hintText: 'Name',
+                                    labelText: 'Name',
+                                  ),
                                 ),
                               ),
-                            ),
-                            SizedBox(
-                              height: 10,
-                            ),
-                            //type of customer
-                            Padding(
-                              padding: EdgeInsets.symmetric(vertical: 5),
-                              child: TextFormField(
-                                decoration: InputDecoration(
-                                  border: OutlineInputBorder(),
-                                  hintText: 'Type',
-                                  labelText: 'Type',
+                              SizedBox(
+                                height: 10,
+                              ),
+                              //location
+                              Padding(
+                                padding: EdgeInsets.symmetric(vertical: 5),
+                                child: TextFormField(
+                                  decoration: InputDecoration(
+                                    border: OutlineInputBorder(),
+                                    hintText: 'Location',
+                                    labelText: 'Location',
+                                  ),
                                 ),
                               ),
-                            ),
-                            SizedBox(
-                              height: 10,
-                            ),
-                            Padding(
-                              padding: EdgeInsets.symmetric(vertical: 5),
-                              child: TextFormField(
-                                maxLines: 5,
-                                decoration: InputDecoration(
-                                  border: OutlineInputBorder(),
-                                  hintText: 'About',
-                                  labelText: 'About...',
-                                  hintMaxLines: 5,
+                              SizedBox(
+                                height: 10,
+                              ),
+                              //type of customer
+                              Padding(
+                                padding: EdgeInsets.symmetric(vertical: 5),
+                                child: TextFormField(
+                                  decoration: InputDecoration(
+                                    border: OutlineInputBorder(),
+                                    hintText: 'Type',
+                                    labelText: 'Type',
+                                  ),
                                 ),
                               ),
-                            ),
-                          ],
+                              SizedBox(
+                                height: 10,
+                              ),
+                              Padding(
+                                padding: EdgeInsets.symmetric(vertical: 5),
+                                child: TextFormField(
+                                  maxLines: 5,
+                                  decoration: InputDecoration(
+                                      border: OutlineInputBorder(),
+                                      hintText: 'About',
+                                      labelText: 'About',
+                                      hintMaxLines: 5,
+                                      alignLabelWithHint: true),
+                                ),
+                              ),
+                            ],
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      GestureDetector(
-                        onTap: () {
-                          //Todo: Tanishk
-                        },
-                        child: Container(
-                          height: 50,
-                          width: 280,
-                          margin: EdgeInsets.symmetric(vertical: 20),
-                          padding: EdgeInsets.symmetric(vertical: 5.0),
-                          alignment: Alignment.center,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(50),
-                            color: kButtonColor,
-                          ),
-                          child: Text(
-                            'SAVE DETAILS',
-                            style: GoogleFonts.urbanist(
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold,
-                              color: kLightColor,
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        GestureDetector(
+                          onTap: () {
+                            //Todo: Tanishk
+                          },
+                          child: Container(
+                            height: 50,
+                            width: 280,
+                            margin: EdgeInsets.symmetric(vertical: 20),
+                            padding: EdgeInsets.symmetric(vertical: 5.0),
+                            alignment: Alignment.center,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(50),
+                              color: kButtonColor,
+                            ),
+                            child: Text(
+                              'SAVE DETAILS',
+                              style: GoogleFonts.urbanist(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                                color: kLightColor,
+                              ),
                             ),
                           ),
                         ),
-                      ),
-                    ],
-                  ),
-                ],
+                      ],
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
